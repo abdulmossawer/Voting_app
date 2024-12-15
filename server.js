efb5aec482e00d0490e3a16b4579140149b08b8d
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2024-12-15 20:39:10 +0530

    Testing Rate limiter

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const rateLimit = require("express-rate-limit")
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
+// middleware for rate limiter
+let limiter = rateLimit({
+    max: 2,
+    windowMs: 60 * 60 * 1000,
+    message: 'We have recived to many request from this IP. Please try after one hour'
+})
+
+app.use("/candidate", limiter)
 
 // const {jwtAuthMiddleware} = require('./jwt');
 
